769406522814c3c3fcd348c707712b3480ec3bb0
DECORTES52 <89760372+DECORTES52@users.noreply.github.com>
2022-10-01 11:42:54 -0500

    Update rp2_sim.py
    comentarios de la funciones por Daniel Cortes

--- rp2_sim.py
+++ rp2_sim.py
@@ -1,7 +1,10 @@
+#Daniel Cortes, esta es la funcion la cual recibira un arreglo
+#Daniel Cortes*args el cual tendra distinto valores, con el **kwargs elegira los parametros
+# Daniel Cortes los cuales apareceran en pantalla
 def asm_pio(*args, **kwargs):
     def decorador(programa):
         def compilador():
-            print("Parámetros", kwargs) 
+            print("Parámetros", kwargs)
             programa()
             return None
         return compilador
@@ -10,14 +13,14 @@ def asm_pio(*args, **kwargs):
 def decorador_instr(fun_inst):
     def decoracion_instr(self,*args, **kwargs):
         fun_inst(self,*args, **kwargs)
-        return None 
+        return None
     return decoracion_instr
 
 pins='pins'
 
 class PIO():
     OUT_LOW='PIO.OUT_LOW'
-    
+    #daniel cortes, constructor sirve para llamar a la clase PIO
 
 class StateMachine:
   def __init__(self, id_, program, freq=125000000, **kwargs):
@@ -28,18 +31,22 @@ class StateMachine:
         program()
         print('Fueron leidas',len(self.lista_instr), 'instrucciones')
         sm_iniciandose=None
+        #Daniel Cortes; sobra igualar sm_iniciandose= self; porque
+        #Daniel Cortes; no lo estamos usando y al final de esta funcion termina en su estado inicial
         fsms[id_]=self
         pass
-      
-        
+      #Daniel Cortes; en esta funcion se recibne los parametros
+      #Daniel Cortes; guardando e imprimiendo el valor de self en una variable global "fsms"
+
   def active(self, x=None):
     '''Esta rutina simula exclisivamnte esa FSM. Sería interesante crear simulación en parlelo con otras FSM'''
     if x==1:
         print('Está pendiente de realizar la simulacón')
+    #Daniel Cortes; la simulacion seria un buen objeto de practica, pero que se vean con una interfaz grafica :P
 
 fsms=[None]*8
 
-sm_iniciandose=None    
+sm_iniciandose=None
 
 
 class nop:
@@ -48,26 +55,29 @@ class nop:
         global sm_iniciandose
         print(self.__class__.__name__)#,'nop.__init__',args,kwargs)
         sm_iniciandose.lista_instr.append(self)
-
+        #Daniel Cortes imprimira el self y el nombre de la clase
         pass
-     
+
     def __getitem__(self,name):
         #print('nop.__getattr__',name)
         pass
-        
+        #Daniel Cortes, no esta en funcionamiento ya que la unica accion accion
+        #Daniel Cortes; que realizaba  esta en comentario
 class set(nop):
     def __init__(self,*args, **kwargs):
         super().__init__(*args, **kwargs)
         pass
-   
+   #Daniel Cortes esta clase hereda las condiciones de la clase nop,
+   #Daniel Cortes pero aparecera impresa la clase set
 class wrap_target(nop):
     def __init__(self,*args, **kwargs):
          super().__init__(*args, **kwargs)
-         pass 
-  
+         pass
+   #Daniel Cortes esta clase hereda las condiciones de la clase nop,
+   #Daniel Cortes pero aparecera impresa la clase wrap_traget
 class wrap(nop):
     def __init__(self,*args, **kwargs):
          super().__init__(*args, **kwargs)
-         pass 
-         
-         
+         pass
+    #Daniel Cortes esta clase hereda las condiciones de la clase nop,
+    #Daniel Cortes pero aparecera impresa la clase wrap
