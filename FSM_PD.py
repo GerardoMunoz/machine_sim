40c39411ca3e7f5fb75db347d44ade28dc375fdf
Oscar Poblador <odpobladorp@correo.udistrital.edu.co>
2022-10-22 11:50:24 -0500

    Pruebas iloc

--- FSM_PD.py
+++ FSM_PD.py
@@ -1,105 +1,112 @@
-import pandas as pd1
+#import pandas as pd1
 import pd_sim as pd2
 
 nombre_archivo = 'FSM_dosUnosSeguidos.csv'
 
-data_frame1 = pd1.read_csv(nombre_archivo)
+#data_frame1 = pd1.read_csv(nombre_archivo)
 data_frame2 = pd2.read_csv(nombre_archivo)
 
-print(data_frame1)
-print(data_frame2)
+# #print(data_frame1)
+# print(data_frame2)
 
-print()
+# print()
 
-print(data_frame1.head())
+#print(data_frame1.head())
 #error print(data_frame2.head())
 
-print()
+# print()
 
-print(data_frame1.tail())
-#error print(data_frame2.tail())
+# #print(data_frame1.tail())
+# #error print(data_frame2.tail())
 
-print()
+# print()
 
-print(data_frame1.index)
-#error print(data_frame2.index)
+# #print(data_frame1.index)
+# #error print(data_frame2.index)
 
-print()
+# print()
 
-print(data_frame1.describe())
-#error print(data_frame2.describe())
+# #print(data_frame1.describe())
+# #error print(data_frame2.describe())
 
-print()
+# print()
 
-print(data_frame1.T)
-print(data_frame2.T)
-#error print(data_frame2.T())
+# #print(data_frame1.T)
+# print(data_frame2.T)
+# #error print(data_frame2.T())
 
-print()
+# print()
 
-print(data_frame1.sort_index(axis=1, ascending=False))
-#error print(data_frame2.sort_index(axis=1, ascending=False))
+# #print(data_frame1.sort_index(axis=1, ascending=False))
+# #error print(data_frame2.sort_index(axis=1, ascending=False))
 
-print()
+# print()
 
-print(data_frame1['Est_act'])
-print(data_frame2['Est_act'])
+# #print(data_frame1['Est_act'])
+# #print(data_frame2['Est_act'])
 
-print()
+# print()
 
-print(data_frame1[0:3])
-print(data_frame2[0:3])
+# #print(data_frame1[0:3])
+# #print(data_frame2[0:3])
 
-print()
+# print()
 
-print(data_frame1.loc[0])
-#error print(data_frame2.loc[0])
-#error print(data_frame2.loc(0))
+# #print(data_frame1.loc[0])
+# #error print(data_frame2.loc[0])
+# #error print(data_frame2.loc(0))
 
-print()
+# print()
 
-print(data_frame1.at[1, "Est_act"])
+#print(data_frame1.at[1, "Est_act"])
 #error print(data_frame2.at[1, "Est_act")])
 #error print(data_frame2.at(1, "Est_act"))
 
-print()
+# print()
 
-print(data_frame1 > 0)
-#error print(data_frame2 > 0)
+# #print(data_frame1 > 0)
+# #error print(data_frame2 > 0)
 
-print()
+# print()
 
-print(data_frame1.iloc[3])
-#error print(data_frame2.iloc[3])
-#error print(data_frame2.iloc(3))
+# #print(data_frame1.iloc[3])
+# #error print(data_frame2.iloc[3])
+# #error print(data_frame2.iloc(3))
 
-print()
+# print()
 
-data_frame1_a=data_frame1.copy()
-print(data_frame1_a)
-#error data_frame2_a=data_frame2.copy()
-#error print(data_frame2_a)
+# #data_frame1_a=data_frame1.copy()
+# #print(data_frame1_a)
+# #error data_frame2_a=data_frame2.copy()
+# #error print(data_frame2_a)
 
-print()
+# print()
 
-data_frame1_a['Est_act'] = 0
-print(data_frame1_a)
-#data_frame2['Est_act'] = 0
-#print(data_frame2)
+# #data_frame1_a['Est_act'] = 0
+# #print(data_frame1_a)
+# #data_frame2['Est_act'] = 0
+# #print(data_frame2)
 
-print('Función negar -, Gerardo Muñoz')
+# print('Función negar -, Gerardo Muñoz')
 
-print(-data_frame1)
-print(-data_frame2)
+# #print(-data_frame1)
+# print(-data_frame2)
 
-print()
+# print()
 
-#Test Oscar-Poblador116
+
 #Oscar David Poblador Parra - 20211005116
 #Función de selección de datos Iloc
-
-data_frame2.iloc[4]
-data_frame1.iloc[3:5,0:2]
+print("\nIloc selección entero y slice - Oscar Poblador 20211005116")
+print("\nPRUEBA CON PÁRAMETRO ENTERO")
+data_frame2.iloc[2]
+print("\n")
+print("\nValores seleccionados")
+print(data_frame2.iloc.seleccion_titulo)
+print(data_frame2.iloc.seleccion_valor)
+print("\nPRUEBA CON PÁRAMETRO SLICE")
+data_frame2.iloc[1:3,0:2]
+print("\nValores seleccionados")
 print("\n")
 print(data_frame2.iloc.seleccion_titulo)
 print(data_frame2.iloc.seleccion_valor)
